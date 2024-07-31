275ffd689592226c3f770679f172377ce2546b67
david <mrminecraft00@gmail.com>
2024-07-31 16:23:23 +0200

    Update build.gradle.kts dependencies and configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,101 +1,38 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import com.diffplug.gradle.spotless.SpotlessPlugin
-import org.ajoberstar.grgit.Grgit
-
 plugins {
-    java
-
-    id("com.diffplug.spotless") version "6.25.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("org.ajoberstar.grgit") version "5.2.2"
-
-    idea
-    eclipse
+    id("java")
+    id("io.github.goooler.shadow") version "8.1.7"
 }
 
-the<JavaPluginExtension>().toolchain {
-    languageVersion.set(JavaLanguageVersion.of(17))
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
-tasks.compileJava.configure {
-    options.release.set(8)
-}
-
-configurations.all {
-    attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
+tasks.compileJava {
+    options.release.set(21)
 }
 
 repositories {
     mavenCentral()
-    maven {
-        name = "Paper"
-        url = uri("https://papermc.io/repo/repository/maven-public/")
-    }
-    maven {
-        name = "Mojang"
-        url = uri("https://libraries.minecraft.net/")
-    }
+    maven("https://jitpack.io")
+    maven("https://repo.thenextlvl.net/releases")
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
-    compileOnly("com.mojang:authlib:1.5.25")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    compileOnly("net.thenextlvl.core:annotations:2.0.1")
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+
     implementation("org.bstats:bstats-bukkit:3.0.2")
-    implementation("org.bstats:bstats-base:3.0.2")
-    implementation("com.github.cryptomorin:XSeries:11.2.0")
-    implementation("dev.notmyfault.serverlib:ServerLib:2.3.6")
-    implementation("io.papermc:paperlib:1.0.8")
-    compileOnly("org.apache.logging.log4j:log4j-api:2.23.1")
+    implementation("net.thenextlvl.core:adapters:1.0.9")
+    implementation("net.thenextlvl.core:paper:1.4.1")
+    implementation("net.thenextlvl.core:files:1.0.5")
+    implementation("net.thenextlvl.core:i18n:1.0.18")
+
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
 
-var buildNumber by extra("")
-ext {
-    val git: Grgit = Grgit.open {
-        dir = File("$rootDir/.git")
-    }
-    val commit: String? = git.head().abbreviatedId
-    buildNumber = if (project.hasProperty("buildnumber")) {
-        project.properties["buildnumber"] as String
-    } else {
-        commit.toString()
-    }
-}
-
-version = String.format("%s-%s", rootProject.version, buildNumber)
-
-tasks.named<ShadowJar>("shadowJar") {
-    archiveClassifier.set(null as String?)
-    dependencies {
-        relocate("com.cryptomorin.xseries", "net.arcaniax.buildersutilities.xseries") {
-            include(dependency("com.github.cryptomorin:XSeries:11.2.0"))
-        }
-        relocate("org.bstats", "net.arcaniax.buildersutilities.metrics") {
-            include(dependency("org.bstats:bstats-base:3.0.2"))
-            include(dependency("org.bstats:bstats-bukkit:3.0.2"))
-        }
-        relocate("io.papermc.lib", "net.arcaniax.buildersutilities.paperlib") {
-            include(dependency("io.papermc:paperlib:1.0.8"))
-        }
-        relocate("org.incendo.serverlib", "net.arcaniax.buildersutilities.serverlib") {
-            include(dependency("dev.notmyfault.serverlib:ServerLib:2.3.6"))
-        }
-    }
+tasks.shadowJar {
+    archiveBaseName.set("creative-utilities")
     minimize()
-}
-
-spotless {
-    java {
-        licenseHeaderFile(rootProject.file("HEADER.txt"))
-        target("**/*.java")
-    }
-}
-
-tasks.named<Copy>("processResources") {
-    filesMatching("plugin.yml") {
-        expand("version" to project.version)
-    }
-}
-
-tasks.named("build").configure {
-    dependsOn("shadowJar")
 }
