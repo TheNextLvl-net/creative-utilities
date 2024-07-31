9e2e641b4b1f3fe50a8e6b76b25ce03c2851d70c
david <mrminecraft00@gmail.com>
2024-07-31 17:00:29 +0200

    [ci skip] Refactor item creation

--- src/main/java/net/thenextlvl/utilities/util/BannerUtil.java
+++ src/main/java/net/thenextlvl/utilities/util/BannerUtil.java
@@ -18,85 +18,32 @@
  */
 package net.thenextlvl.utilities.util;
 
-import com.cryptomorin.xseries.XMaterial;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.DyeColor;
+import org.bukkit.Material;
 import org.bukkit.block.banner.Pattern;
 import org.bukkit.block.banner.PatternType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BannerMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 public class BannerUtil {
 
     public static final HashMap<UUID, ItemStack> currentBanner = new HashMap<>();
     public static final HashMap<UUID, DyeColor> selectedColor = new HashMap<>();
 
-    public static final List<DyeColor> allColors = new ArrayList<>();
-    public static final List<PatternType> allPatterns = new ArrayList<>();
+    public static final List<DyeColor> allColors = Arrays.stream(DyeColor.values())
+            .toList();
+    public static final List<PatternType> allPatterns = Arrays.stream(PatternType.values())
+            .filter(patternType -> !patternType.equals(PatternType.BASE))
+            .toList();
     private static final Random random = new Random();
-
-    public static void addPatterns() {
-        for (PatternType pt : PatternType.values()) {
-            if (!pt.equals(PatternType.BASE)) {
-                allPatterns.add(pt);
-            }
-        }
-
-        Class<?> craftPatternTypeClass;
-        Method bukkitToMinecraftHolder;
-        try {
-            String craftBukkitPackage = Bukkit.getServer().getClass().getPackage().getName();
-            craftPatternTypeClass = Class.forName(craftBukkitPackage + ".block.banner.CraftPatternType");
-            bukkitToMinecraftHolder = craftPatternTypeClass.getMethod("bukkitToMinecraftHolder", PatternType.class);
-        } catch (ClassNotFoundException | NoSuchMethodException ignored) {
-            // CraftPatternType class was only added in 1.20.5
-            return;
-        }
-
-        allPatterns.removeIf(pt -> {
-            try {
-                // Attempt to find pattern in banner pattern registry
-                bukkitToMinecraftHolder.invoke(craftPatternTypeClass, pt);
-                return false;
-            } catch (InvocationTargetException | IllegalAccessException ignored) {
-                // Pattern is not present in registry, thus remove it
-                return true;
-            }
-        });
-    }
-
-    public static void addColors() {
-        allColors.add(DyeColor.BLACK);
-        allColors.add(DyeColor.RED);
-        allColors.add(DyeColor.GREEN);
-        allColors.add(DyeColor.BROWN);
-        allColors.add(DyeColor.BLUE);
-        allColors.add(DyeColor.PURPLE);
-        allColors.add(DyeColor.CYAN);
-        allColors.add(DyeColor.LIGHT_GRAY);
-        allColors.add(DyeColor.GRAY);
-        allColors.add(DyeColor.PINK);
-        allColors.add(DyeColor.LIME);
-        allColors.add(DyeColor.YELLOW);
-        allColors.add(DyeColor.LIGHT_BLUE);
-        allColors.add(DyeColor.MAGENTA);
-        allColors.add(DyeColor.ORANGE);
-        allColors.add(DyeColor.WHITE);
-    }
 
     public static ItemStack createBanner(String name, DyeColor base, String lore, List<Pattern> patterns) {
         ItemStack item = Items.create(
-                XMaterial.matchXMaterial(base.toString() + "_BANNER").get().parseMaterial(),
+                Material.matchMaterial(base.toString() + "_BANNER"),
                 (short) 0,
                 1,
                 name,
@@ -105,7 +52,7 @@ public class BannerUtil {
         BannerMeta meta = (BannerMeta) item.getItemMeta();
 
         meta.setPatterns(patterns);
-        if (!lore.equals("")) {
+        if (!lore.isEmpty()) {
             String[] loreListArray = lore.split("__");
             List<String> loreList = new ArrayList<>();
             for (String s : loreListArray) {
@@ -119,7 +66,7 @@ public class BannerUtil {
 
     public static ItemStack createDye(String name, DyeColor base, String lore) {
         ItemStack item = Items.create(
-                XMaterial.matchXMaterial(base.toString() + "_DYE").get().parseMaterial(),
+                Material.matchMaterial(base.toString() + "_DYE"),
                 (short) 0,
                 1,
                 name,
@@ -139,17 +86,15 @@ public class BannerUtil {
     }
 
     public static ItemStack createBanner(String name, DyeColor base, String lore) {
-        XMaterial.matchXMaterial(base.toString() + "_BANNER").get().parseMaterial();
-
         ItemStack item = Items.create(
-                XMaterial.matchXMaterial(base + "_BANNER").get().parseMaterial(),
+                Material.matchMaterial(base + "_BANNER"),
                 (short) 0,
                 1,
                 name,
                 ""
         );
         BannerMeta meta = (BannerMeta) item.getItemMeta();
-        if (!lore.equals("")) {
+        if (!lore.isEmpty()) {
             String[] loreListArray = lore.split("__");
             List<String> loreList = new ArrayList<>();
             for (String s : loreListArray) {
@@ -163,7 +108,7 @@ public class BannerUtil {
 
     public static ItemStack createBanner(String name, int amount, DyeColor base, String lore, Pattern pat) {
         ItemStack item = Items.create(
-                XMaterial.matchXMaterial(base.toString() + "_BANNER").get().parseMaterial(),
+                Material.matchMaterial(base.toString() + "_BANNER"),
                 (short) 0,
                 1,
                 name,
@@ -171,7 +116,7 @@ public class BannerUtil {
         );
         BannerMeta meta = (BannerMeta) item.getItemMeta();
         meta.addPattern(pat);
-        if (!lore.equals("")) {
+        if (!lore.isEmpty()) {
             String[] loreListArray = lore.split("__");
             List<String> loreList = new ArrayList<>();
             for (String s : loreListArray) {
@@ -184,22 +129,12 @@ public class BannerUtil {
     }
 
     public static ItemStack addPattern(ItemStack i, Pattern pat) {
-        if (i.getType().toString().contains("BANNER")) {
-            BannerMeta meta = (BannerMeta) i.getItemMeta();
-            List<Pattern> patterns = meta.getPatterns();
-            patterns.add(pat);
-            meta.setPatterns(patterns);
-            i.setItemMeta(meta);
-            return i;
-        }
-        return null;
+        return i.editMeta(BannerMeta.class, meta -> meta.addPattern(pat)) ? i : null;
     }
 
     public static List<Pattern> getPatterns(ItemStack i) {
-        if (i.getType().toString().contains("BANNER")) {
-            BannerMeta meta = (BannerMeta) i.getItemMeta();
-            List<Pattern> patterns = meta.getPatterns();
-            return patterns;
+        if (i.getItemMeta() instanceof BannerMeta meta) {
+            return meta.getPatterns();
         }
         return new ArrayList<>();
     }
@@ -213,27 +148,10 @@ public class BannerUtil {
     }
 
     public static DyeColor getOppositeBaseColor(DyeColor dyeColor) {
-        switch (dyeColor) {
-            case RED:
-            case BLUE:
-            case CYAN:
-            case GRAY:
-            case BLACK:
-            case BROWN:
-            case GREEN:
-            case PURPLE:
-                return DyeColor.WHITE;
-            case PINK:
-            case WHITE:
-            case ORANGE:
-            case YELLOW:
-            case MAGENTA:
-            case LIGHT_BLUE:
-            case LIGHT_GRAY:
-            case LIME:
-            default:
-                return DyeColor.BLACK;
-        }
+        return switch (dyeColor) {
+            case RED, BLUE, CYAN, GRAY, BLACK, BROWN, GREEN, PURPLE -> DyeColor.WHITE;
+            default -> DyeColor.BLACK;
+        };
     }
 
 }
