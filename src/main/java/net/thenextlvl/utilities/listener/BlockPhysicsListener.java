4f93c2c76a6102d509a41c4ca3ded41f12e36490
david <mrminecraft00@gmail.com>
2024-07-31 20:49:10 +0200

    Refactor BlockPhysicsListener to streamline logic and improve clarity.

--- src/main/java/net/thenextlvl/utilities/listener/BlockPhysicsListener.java
+++ src/main/java/net/thenextlvl/utilities/listener/BlockPhysicsListener.java
@@ -18,95 +18,60 @@
  */
 package net.thenextlvl.utilities.listener;
 
-import net.thenextlvl.utilities.Settings;
-import net.thenextlvl.utilities.util.LogManagerCompat;
-import org.apache.logging.log4j.Logger;
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.utilities.UtilitiesPlugin;
 import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPhysicsEvent;
 
+@RequiredArgsConstructor
 public class BlockPhysicsListener implements Listener {
-
-    private static final Logger logger = LogManagerCompat.getLogger();
+    private final UtilitiesPlugin plugin;
 
     @EventHandler
-    public void onPhysics(BlockPhysicsEvent e) {
-        if (e.getSourceBlock().getType().equals(Material.AIR)) {
-            if (e.getChangedType().equals(Material.AIR)) {
-                if (e.getBlock().getLocation().getBlockY() > 0) {
-                    if (e
-                            .getBlock()
-                            .getLocation()
-                            .add(0, -1, 0)
-                            .getBlock()
-                            .getType()
-                            .name()
-                            .toLowerCase()
-                            .contains("grass_block")) {
-                        return;
-                    }
-                }
-            }
-        }
-        if (e.getSourceBlock().getType().name().toLowerCase().contains("snow")) {
-            if (e.getBlock().getLocation().getBlockY() > 0) {
-                if (e.getBlock().getLocation().add(0, -1, 0).getBlock().getType().name().toLowerCase().contains("grass_block")) {
-                    return;
-                }
-            }
-        }
-        try {
-            if (e.getChangedType().name().toLowerCase().contains("chest") ||
-                    e.getChangedType().name().toLowerCase().contains("stair") ||
-                    e.getChangedType().name().toLowerCase().contains("fence") ||
-                    e.getChangedType().name().toLowerCase().contains("pane") ||
-                    e.getChangedType().name().toLowerCase().contains("wall") ||
-                    e.getChangedType().name().toLowerCase().contains("bar") ||
-                    e.getChangedType().name().toLowerCase().contains("door")) {
+    public void onPhysics(BlockPhysicsEvent event) {
+        if (event.getSourceBlock().isEmpty() && event.getChangedType().isEmpty()) {
+            if (event.getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.GRASS_BLOCK)) {
                 return;
             }
-        } catch (Exception ex) {
+        }
+        if (event.getSourceBlock().getType().equals(Material.SNOW)
+            || event.getSourceBlock().getType().equals(Material.POWDER_SNOW)
+            || event.getSourceBlock().getType().equals(Material.SNOW_BLOCK)) {
+            if (event.getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.GRASS_BLOCK)) {
+                return;
+            }
+        }
+        if (event.getChangedType().name().toLowerCase().contains("chest") ||
+            event.getChangedType().name().toLowerCase().contains("stair") ||
+            event.getChangedType().name().toLowerCase().contains("fence") ||
+            event.getChangedType().name().toLowerCase().contains("pane") ||
+            event.getChangedType().name().toLowerCase().contains("wall") ||
+            event.getChangedType().name().toLowerCase().contains("bar") ||
+            event.getChangedType().name().toLowerCase().contains("door")) {
             return;
         }
-        if (!Settings.disableRedstone) {
-            if (e.getChangedType().name().toLowerCase().contains("redstone") ||
-                    e.getChangedType().name().toLowerCase().contains("daylight") ||
-                    e.getChangedType().name().toLowerCase().contains("diode") ||
-                    e.getChangedType().name().toLowerCase().contains("note") ||
-                    e.getChangedType().name().toLowerCase().contains("lever") ||
-                    e.getChangedType().name().toLowerCase().contains("button") ||
-                    e.getChangedType().name().toLowerCase().contains("command") ||
-                    e.getChangedType().name().toLowerCase().contains("tripwire") ||
-                    e.getChangedType().name().toLowerCase().contains("plate") ||
-                    e.getChangedType().name().toLowerCase().contains("string") ||
-                    e.getChangedType().name().toLowerCase().contains("piston") ||
-                    e.getChangedType().name().toLowerCase().contains("observer")) {
-                if (!e.getBlock().getType().name().contains("air")) {
-                    return;
-                }
+        if (!plugin.config().disableRedstone()) {
+            if (event.getChangedType().name().toLowerCase().contains("redstone") ||
+                event.getChangedType().name().toLowerCase().contains("daylight") ||
+                event.getChangedType().name().toLowerCase().contains("diode") ||
+                event.getChangedType().name().toLowerCase().contains("note") ||
+                event.getChangedType().name().toLowerCase().contains("lever") ||
+                event.getChangedType().name().toLowerCase().contains("button") ||
+                event.getChangedType().name().toLowerCase().contains("command") ||
+                event.getChangedType().name().toLowerCase().contains("tripwire") ||
+                event.getChangedType().name().toLowerCase().contains("plate") ||
+                event.getChangedType().name().toLowerCase().contains("string") ||
+                event.getChangedType().name().toLowerCase().contains("piston") ||
+                event.getChangedType().name().toLowerCase().contains("observer")) {
+                if (!event.getBlock().isEmpty()) return;
             }
         }
 
-        if (e.getChangedType().hasGravity()) {
-            if (Settings.disableGravityPhysics) {
-                e.setCancelled(true);
-                if (Settings.sendDebugMessages) {
-                    logger.info(
-                            "Gravity physics were cancelled because disable-gravity-physics: true");
-                }
-
-            }
-        } else {
-            if (Settings.disablePhysics) {
-                e.setCancelled(true);
-                if (Settings.sendDebugMessages) {
-                    logger.info(
-                            "Physics were cancelled because disable-physics: true");
-                }
-            }
-        }
-
+        event.setCancelled(event.getChangedType().hasGravity()
+                ? plugin.config().disableGravity()
+                : plugin.config().disablePhysics());
     }
-
 }
