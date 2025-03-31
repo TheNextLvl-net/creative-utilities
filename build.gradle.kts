c47990a5886df8616374ddd51d2bf3314533c96d
david <github@nonswag.dev>
2025-03-31 19:59:15 +0200

    Bumped `com.gradleup.shadow` to 9.0.0-beta9
    Updated plugin version in build configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import net.minecrell.pluginyml.paper.PaperPluginDescription
 
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "8.3.6"
+    id("com.gradleup.shadow") version "9.0.0-beta9"
     id("io.papermc.hangar-publish-plugin") version "0.1.3"
     id("net.minecrell.plugin-yml.paper") version "0.6.0"
 }
